266d87c7bb11fa42ebd447083b0a2dc232781cd1
parkjugod <pjh_1987@naver.com>
2016-10-29 15:56:44 +0900

    script

---
+++ UnityStudy/Assets/Scripts/LogoScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LogoScript : MonoBehaviour {
+    public float delayTime = 3;
+
+	// Use this for initialization
+	IEnumerator Start () {
+        yield return new WaitForSeconds(delayTime);
+
+        Application.LoadLevel("GameScene");
+	}
+}
