8dc4fb42cbb1c9b5a9d7e6d359472d96aad91612
lostjc <lostjc001@gmail.com>
2016-10-29 16:38:08 +0900

    블럭 생성

---
+++ UnityStudy/Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject[] blcokPrefab = null;
+    public List<GameObject> blockList = null;
+
+	// Use this for initialization
+	void Start () {
+	    for(int i=0; i<10; i++)
+        {
+            NewBlock(i);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void NewBlock(int y)
+    {
+        int rand = Random.Range(0, 2);
+        Vector2 pos = new Vector2(0, y);
+        GameObject block = Instantiate(blcokPrefab[rand], pos, Quaternion.identity) as GameObject;
+        blockList.Add(block);
+
+    }
+}
